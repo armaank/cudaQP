da8ba2c0e7e4cc1ddcb06e35502e1ecbbf53fc82
armaank <kohli@cooper.edu>
2020-04-24 00:21:39 -0400

    finished projection

---
+++ include/proj.h
@@ -0,0 +1,26 @@
+# include "qptypes.h"
+# include "ops.h"
+
+/* Define Projections onto set C involved in the ADMM algorithm */
+
+/**
+ * Project z onto \f$C = [l, u]\f$
+ * @param z    Vector to project
+ * @param work Workspace
+ */
+void project(qpWorkspace *work,
+             float       *z);
+
+
+/**
+ * Ensure z satisfies box constraints and y is is normal cone of z
+ * @param work Workspace
+ * @param z    Primal variable z
+ * @param y    Dual variable y
+ */
+void project_normalcone(qpWorkspace *work,
+                        float       *z,
+                        float       *y);
+
+
+}
