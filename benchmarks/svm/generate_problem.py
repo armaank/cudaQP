10330a857724e9e7a5844d9e1280f21a70c5abb3
armaank <kohli@cooper.edu>
2020-05-14 14:07:08 -0400

    updated report, cleanup

--- benchmarks/svm/generate_problem.py
+++ benchmarks/svm/generate_problem.py
@@ -16,20 +16,26 @@ gamma = 1.0
 b = np.hstack([np.ones(N), -np.ones(N)])
 A_upp = sparse.random(N, n, density=0.5)
 A_low = sparse.random(N, n, density=0.5)
-Ad = sparse.vstack([
-        A_upp / np.sqrt(n) + (A_upp != 0.).astype(float) / n,
-        A_low / np.sqrt(n) - (A_low != 0.).astype(float) / n
-     ], format='csc')
+Ad = sparse.vstack(
+    [
+        A_upp / np.sqrt(n) + (A_upp != 0.0).astype(float) / n,
+        A_low / np.sqrt(n) - (A_low != 0.0).astype(float) / n,
+    ],
+    format="csc",
+)
 
 # osqp data
 Im = sparse.eye(m)
-P = sparse.block_diag([sparse.eye(n), sparse.csc_matrix((m, m))], format='csc')
-q = np.hstack([np.zeros(n), gamma*np.ones(m)])
-A = sparse.vstack([
+P = sparse.block_diag([sparse.eye(n), sparse.csc_matrix((m, m))], format="csc")
+q = np.hstack([np.zeros(n), gamma * np.ones(m)])
+A = sparse.vstack(
+    [
         sparse.hstack([sparse.diags(b).dot(Ad), -Im]),
-        sparse.hstack([sparse.csc_matrix((m, n)), Im])
-    ], format='csc')
-l = np.hstack([-np.inf*np.ones(m), np.zeros(m)])
-u = np.hstack([-np.ones(m), np.inf*np.ones(m)])
+        sparse.hstack([sparse.csc_matrix((m, n)), Im]),
+    ],
+    format="csc",
+)
+l = np.hstack([-np.inf * np.ones(m), np.zeros(m)])
+u = np.hstack([-np.ones(m), np.inf * np.ones(m)])
 
-cu.generate_problem_data(P, q, A, l, u, 'svm')
+cu.generate_problem_data(P, q, A, l, u, "svm")
